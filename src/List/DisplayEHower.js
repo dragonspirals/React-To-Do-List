bad2cc926861393c6801750bc1416159a742d962
dragonspirals <leeradja99@gmail.com>
2023-06-06 13:43:15 +0100

    Rewrote the displayEHower code

--- src/List/DisplayEHower.js
+++ src/List/DisplayEHower.js
@@ -1,8 +1,6 @@
-
 import { useState, useEffect, useRef } from "react";
 
 export default function DisplayEHower({ taskList }) {
-    
 
     const boxRef = useRef(null);
 
@@ -13,56 +11,26 @@ export default function DisplayEHower({ taskList }) {
     }, [])
 
 
-    function PlotMarks({ taskList }) {
-
-
-        // marks will keep a list of the position of the marks
-        const marks = [];
-
-        taskList.forEach(task => {
-            if (task.hasEHower && boxRef.current) {
-                const markX = ((100-task.eHower[0])*boxSize[0])/100;
-                const markY = ((100-task.eHower[1])*boxSize[1])/100;
-
-                marks.push([markX, markY]);
-            }
-        })
-
-
+    function PlotMark({ task, taskIndex, boxSize }) {
         
-
-
-        function PlotMark({markPos, markIndex}) {
-
-
-            return (
-                <>
-                <svg display="block" style={{transform: `translateY(-${markIndex*100}%)`}} className="e-hower-plot"  height="100%" width="100%">
-                    <circle cx={markPos[0]} cy={markPos[1]} r="2" stroke="black" stroke-width="2" fill="black" />
-                </svg>
-                </>
-            )
-        }
-
+        const markX = ((100-task.eHower[0])*boxSize[0])/100;
+        const markY = ((100-task.eHower[1])*boxSize[1])/100;
 
 
         return (
-            marks.map((newMark, index) => (
-                <>
-                <PlotMark markPos={newMark} markIndex={index} />
-                </>
-            ))
+            <svg display="block" style={{transform: `translateY(-${taskIndex*100}%)`}} className="e-hower-plot"  height="100%" width="100%">
+                <circle cx={markX} cy={markY} r="2" stroke="black" strokeWidth="2" fill="black" />
+            </svg>
         )
     }
 
-    
+
 
     return (
-        <>      
         <div ref={boxRef} className = "clickable e-hower-div" >
-            {boxRef && <PlotMarks taskList={taskList} boxSize={0.3*window.innerWidth}/>}
+            {taskList.map((element, index) => (
+                element.hasEHower && <PlotMark key={element.id + "mark"} task={element} taskIndex={index} boxSize={boxSize}/>
+            ))}
         </div>
-        <br />
-        </>
     )
 }
